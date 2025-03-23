be0c4a01348dc63cda93ba535b8af92aed777806
Hasse <120029840+Whauu@users.noreply.github.com>
2025-03-23 14:32:46 +0100

    Improved tare
    Made it more responsive

--- src/scale.cpp
+++ src/scale.cpp
@@ -18,10 +18,32 @@ void setupScale(){
 }
 
 void tareScale(){
-  delay(500);
-  scale.tare();
-}
+  //delay(500);
+  int times = 20;
+	long sum = 0;
+  long lastSum = 0;
+  boolean finished = false;
+  int stableCounter = 0;
 
+	for (byte i = 0; i < times && !finished; i++) {
+		sum = scale.read();
+    if (sum == lastSum) {
+      stableCounter++;
+      if (stableCounter >= 2) {
+        finished = true;
+      }
+    }
+    else {
+      stableCounter = 0;
+    }
+    lastSum = sum;
+    scale.set_offset(sum); // Set the scale to 0.0
+    
+		// Probably will do no harm on AVR but will feed the Watchdog Timer (WDT) on ESP.
+		// https://github.com/bogde/HX711/issues/73
+		delay(0);
+	}
+}
 float updateScale(){
   return scale.get_units();
 }
